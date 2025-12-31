8bb87f3c807979dbba9de796c38ebfeba5e98eae
Abdul Majid <majidsaddiqye@gmail.com>
2026-01-01 00:11:49 +0500

    Create authentication using clerk

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import { assets } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 import { ArrowRight } from "lucide-react";
+import { useClerk, useUser, UserButton } from "@clerk/clerk-react";
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const { user } = useUser();
+  const { openSignIn } = useClerk();
+
   return (
     <div className="fixed z-5 w-full backdrop-blur-2xl flex justify-between items-center py-3 px-4 sm:px-20 xl:px-32">
       <img
@@ -13,10 +17,17 @@ const Navbar = () => {
         className="w-32 sm:w-44 cursor-pointer"
         onClick={() => navigate("/")}
       />
-
-      <button className="flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5">
-        Get started <ArrowRight className="w-4 h-4" />
-      </button>
+      {/* if user login show userbutton otherwise show get started button */}
+      {user ? (
+        <UserButton />
+      ) : (
+        <button
+          onClick={openSignIn}
+          className="flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5"
+        >
+          Get started <ArrowRight className="w-4 h-4" />
+        </button>
+      )}
     </div>
   );
 };

8a1b7908f06ff646cf514c833648a25c0d625361
Abdul Majid <majidsaddiqye@gmail.com>
2025-12-31 23:37:18 +0500

    create Routing in app.jsx

--- Client/src/pages/Layout.jsx
+++ Client/src/pages/Layout.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
+import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
     <div>
-        <h1>Layout</h1>
+      <h1>Layout</h1>
+      <Outlet />
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
